7a5e2ce8f3748820e8106e0387733ace4859ff37
SongJinfeng <song.jinfeng@outlook.com>
2022-09-30 10:21:15 +0800

    add local search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,17 @@ const config = {
       }),
     ],
   ],
-
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        language: ["en", "zh"],
+      },
+    ],
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
